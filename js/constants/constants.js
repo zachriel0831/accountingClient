4eca36140ef19b1f152417698c1fd279bfb52b5a
zack ou <tc101fubonzack@gmail.com>
2019-07-14 21:55:32 +0800

    add constant

--- js/constants/constants.js
+++ js/constants/constants.js
@@ -3,3 +3,6 @@ const LOGGED_IN_PAGE = ['/LogOut','/Home','/NtdTransfer_1'];
 const LOGGED_OUT_PAGE = ['/Login','/Register'];
 
 const CATEGORY = ['food','travel','insurance','3C','home','gwen deposit','zack deposit'];
+
+// const CONNECTION_CONFIG = 'https://zachriel-accountting.herokuapp.com';
+const CONNECTION_CONFIG = 'http://localhost:3000';
